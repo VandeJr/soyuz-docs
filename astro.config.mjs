3e3bcc2e8e30432a966520ecbf22a2c2c7984500
VandeJr <vanderlei.jordao.jr@outlook.com>
2026-05-30 10:18:02 -0300

    Publish Soyuz documentation project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Soyuz",
+      description:
+        "Documentação oficial da linguagem de programação Soyuz (.sy)",
+      defaultLocale: "pt-br",
+      social: {
+        github: "https://github.com/VandeJr/soyuz-docs",
+      },
+      sidebar: [
+        {
+          label: "Começando",
+          items: [{ label: "Introdução", link: "/" }],
+        },
+        {
+          label: "A Linguagem",
+          items: [{ autogenerate: { directory: "features" } }],
+        },
+        {
+          label: "Arquitetura",
+          items: [{ autogenerate: { directory: "internals" } }],
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
 });
